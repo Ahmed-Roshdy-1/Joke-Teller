32380dcd596bdb48510663c7a7b97dec289e05a5
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-08-06 21:58:21 +0200

    push commit

--- js/script.js
+++ js/script.js
@@ -109,13 +109,18 @@ var VoiceRSS = {
   },
 }; //  End of VoiceRSS Javascript SDK
 
+// Disable / Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
 // Passing Joke to VoiceRSS API
 function tellme(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
   VoiceRSS.speech({
     key: "83e66ec2d2b14869aa6c3c70391f8f82",
-    src: joke,
+    src: jokeString,
     hl: "en-us",
-    v: "Linda",
+    v: "Mary",
     r: 0,
     c: "mp3",
     f: "44khz_16bit_stereo",
@@ -133,11 +138,14 @@ async function getJokes() {
     const response = await fetch(apiUrl);
     const data = await response.json();
     if (data.setup) {
-      joke = `${data.setup}...${data.delivery}`;
+      joke = `${data.setup} ... ${data.delivery}`;
     } else {
       joke = data.joke;
     }
+    // Text-to-Speech
     tellme(joke);
+    // Disable Button
+    toggleButton();
   } catch (error) {
     // Catch an Error
     console.log("the Error is :", error);
@@ -145,4 +153,5 @@ async function getJokes() {
 }
 
 // Event Listeners
-button.addEventListener("click", getJokes());
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
